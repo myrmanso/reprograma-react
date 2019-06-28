94175d22ac6a98bd70db75ce0c2c6628cc4e3157
myrmanso <mayaramanso.devel@gmail.com>
2019-06-28 16:51:34 -0300

    atualização da pasta semana 03

--- semana_03/back/api/_data/loadInitialData.js
+++
@@ -1,6 +0,0 @@
-function loadInitialData(db) {
-    db.usuarios.insert(require('./usuarios.json'))
-    db.tweets.insert(require('./tweets.json'))
-}
-
-module.exports = (db) => loadInitialData(db)
--- semana_03/back/api/routes/hashtags.js
+++
@@ -1,7 +0,0 @@
-module.exports = (app) => {
-    const hashtagsController = app.controllers.HashtagsController
-
-    app.get('/hashtags', hashtagsController.listar)
-
-    app.get('/hashtags/:hashtag', hashtagsController.listarUma)
-}
